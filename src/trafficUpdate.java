9ff49c089851f86bc5f437bd7835ddab1168c55b
wolfieorama <wachirawamunyi@gmail.com>
2015-07-20 15:38:42 +0300

    commit 2 with options for all roads or specific roads added

--- src/trafficUpdate.java
+++ src/trafficUpdate.java
@@ -1,39 +1,62 @@
 import java.util.Scanner;
 public class trafficUpdate {
 	public static void main(String[] args){
-		System.out.println("We have status updates on the following roads:");
-		System.out.println("r1.Ngong Road r2.Waiyaki Way r3.Mombasa Road r4.Mbagathi Way r5.Jogoo Road");
+		System.out.println("We have traffic status updates for Nairobi");
 		
-//		String r1 = "Yes"; 
-//		String r2 = "No"; 
-//		String r3 = "Yes";
-//		String r4 = "No";
-//		String r5 = "Yes";
+		Scanner choices = new Scanner (System.in);
+		System.out.println("Do you want an update for specific or all roads ?");
+		int y = choices.nextInt();
 		
-		Scanner road = new Scanner (System.in);
-		System.out.println("Enter the road number:");
+		if( y == 1){
+			
+				Scanner road = new Scanner (System.in);
+				System.out.println("Enter the road number:");
+				
+				int x = road.nextInt();
+				
+				if(x == 1){
+					System.out.println("There is traffic on Ngong Road.");
+					
+				}
+				if(x == 2){
+					System.out.println("There is no traffic on Waiyaki Road.");
+					
+				}
+				if(x == 3){
+					System.out.println("There is traffic on Mombasa Road.");
+					
+				}
+				if(x == 4){
+					System.out.println("There is no traffic on Mbagathi Road.");
+					
+				}
+				if(x == 5){
+					System.out.println("There is traffic on Jogoo Road.");
+					
+				}
+			}	
+			
+			else if (y == 2){
+					
+					Scanner allroads = new Scanner(System.in);
+					System.out.println("Do you want roads with or without traffic?");
+					
+					int z = allroads.nextInt();
+					
+					
+					if(z == 1) {
+						System.out.println("The following have traffic:");
+						System.out.println("1.Ngong Road 2.Mombasa Road 3. Jogoo Road");
+					}
+					else{
+						System.out.println("The following have no traffic:");
+						System.out.println("1.Waiyaki Way 2. Mbagathi Road");
+				}
+			
+			
+		}
 		
-		int x = road.nextInt();
+
 		
-		if(x == 1){
-			System.out.println("There is traffic on Ngong Road.");
-			
-		}
-		if(x == 2){
-			System.out.println("There is traffic on Waiyaki Road.");
-			
-		}
-		if(x == 3){
-			System.out.println("There is traffic on Mombasa Road.");
-			
-		}
-		if(x == 4){
-			System.out.println("There is traffic on Mbagathi Road.");
-			
-		}
-		if(x == 5){
-			System.out.println("There is traffic on Jogoo Road.");
-			
-		}
 	}
 }
